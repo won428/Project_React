e8ba162bc64fd66ca6b433dec42ae166485f4f64
[JIN] <[akjlh00@gmail.com]>
2025-10-20 18:06:31 +0900

    '10/20'

--- src/App.js
+++ src/App.js
@@ -4,23 +4,48 @@ import Menuitem from './ui/Menuitems'
 import LoginPage from './pages/LoginPage'
 
 import AppRoutes from './routes/AppRoutes'
+import axios from 'axios';
+import { useState } from 'react';
+import UserProvider from './UserProvider';
 
 
 function App() {
+  const [user, setUser] = useState('');
   const appName = ' LMS Service';
-  {/* {isAuthenticated ? <AppRoute /> : <LoginPage />} JWT token을 사용하여 
-    로그인 확인 + security 적용
-    */ }
+  /**
+   * isAuthentivated 원리
+   * 1. 사용자가 로그인 요청
+   * 2.서버가 사용자 인증 후 JWT(token) 생성
+   * 3. 클라이언트가 JWT 저장
+   * 4. API 요청 시 JWT 첨부
+   * 5. 서버(로컬)에서 JWT 검증
+   * 6. 인증된 유저로 API 응답
+   */
+  // Switch 구문으로 axios respone 받아서 
+  const isAuthenticated = localStorage.getItem("accessToken");
+  console.log(isAuthenticated);
+
+
+
   return (
-    <div>
-      <header className="app-wrapper"><Menuitem /></header>
-      <main className='main-content'>
-        <AppRoutes />
-      </main>
-      <footer className="bg-dark footer py-3 mt-5">
-        <p>&copy;2025{appName}.All rights reserved</p>
-      </footer>
-    </div>
+    <UserProvider>
+      {isAuthenticated ?
+        <div>
+
+          < header className="app-wrapper" > <Menuitem /></header >
+
+          <main className='main-content'>
+            <AppRoutes />
+          </main>
+
+          <footer className="bg-dark footer py-3 mt-5">
+            <p>&copy;2025{appName}.All rights reserved</p>
+          </footer>
+
+        </div >
+
+        : <LoginPage />}
+    </UserProvider>
   );
 }
 
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,116 @@
-
+import { useContext, useState } from "react";
+import { Alert, Button, Card, CardBody, Col, Container, Form, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { API_BASE_URL } from "../config/config";
+import axios from "axios";
+import { UserContext } from "../UserContext";
 
 function App() {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [error, setError] = useState();
+    const { setUser } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    const LoginAction = async (evt) => {
+        evt.preventDefault();
+        try {
+            const url = `${API_BASE_URL}/login`
+            const parameters = new URLSearchParams();
+
+            parameters.append('email', email)
+            parameters.append('password', password)
+
+
+            console.log(parameters);
+            console.log(email);
+            console.log(password);
+
+
+            const respone = await axios.post(
+                url, parameters, {
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            }
+            )
+            console.log(respone);
+
+            const { msg, user, token } = respone.data;
+            console.log(token);
+
+            if (msg === 'success') {
+                localStorage.setItem("accessToken", token);
+                setUser(user);//전역 변수 설정으로 관리
+                navigate(`/`)
+            } else {
+                setError(msg)
+            }
+        } catch (error) {
+            if (error.response) {
+
+
+                setError(error.response.data.message || 'Failed to login');
+                console.log(error);
+            } else {
+                setError('Sever Error');
+            }
+        }
+
+
+    }
+
+
+
     return (
         <>
+            <Container className="d-flex justify-content-center align-items-center">
+                <Row className="w-100 justify-content-center">
+                    <Col md={6} >
+                        <Card  >
+                            <CardBody onSubmit={LoginAction}>
+                                <h3>Login</h3>
+                                {error && <Alert variant="danger">{error}</Alert>}
+                                <Form>
+                                    <Form.Group>
+                                        <Form.Label>
+                                            Email or 학번
+                                        </Form.Label>
+                                        <Form.Control
+                                            type="email"
+                                            placeholder="Email을 입력하세요"
+                                            value={email}
+                                            onChange={(evt) => setEmail(evt.target.value)}
+                                            required
+                                        />
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label>
+                                            Password
+                                        </Form.Label>
+                                        <Form.Control
+                                            type="password"
+                                            placeholder="password를 입력하세요"
+                                            value={password}
+                                            onChange={(evt) => setPassword(evt.target.value)}
+                                            required
+                                        />
+                                    </Form.Group>
+                                    <Row >
+                                        <Col className="align-item-end">
+                                            <Button
+                                                variant="primary"
+                                                type="submit"
+                                                className="w-100"
 
+                                            >Login</Button>
+                                        </Col>
+
+                                    </Row>
+                                </Form>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
