3d51554bb8874e64c4431510fb216b82b94c3fa2
csw3071 <csw30710@naver.com>
2025-10-21 15:38:15 +0900

    25.10.21 사용자등록 1차

--- src/config/config.js
+++ src/config/config.js
@@ -5,4 +5,4 @@ const API_HOST = "localhost";
 
 const API_PORT = "9100";
 
-export const API_BASE_URL = `https://${API_HOST}:${API_PORT}`
+export const API_BASE_URL = `http://${API_HOST}:${API_PORT}`
--- src/pages/Integrated_Info/Insert_User.js
+++ src/pages/Integrated_Info/Insert_User.js
@@ -8,27 +8,29 @@ import axios from "axios";
     function App() {
         const dateInputRef = useRef(null);
         const [user, setUser] = useState({
-            name:'',
-            id:'',
+            u_name:'',
             password:'',
             birthdate:'',
             email:'',
             phone:'',
             gender:'',
             major:'',
-            type:'',
-            status_id:''
+            u_type:'',
             });
-        const[colleges,setColleges] = useState([]);
-        const[majors,setMajors] = useState([]);
+        const[collegeList,setCollegeList] = useState([]);
+        const[majorList,setMajorList] = useState([]);
+        const[college,setCollege] = useState('');
+        
+        
             
         
-        const collegeList = () => {
+        const getCollegeList = () => {
             const url = `${API_BASE_URL}/college/list`
             axios
                 .get(url)
                 .then((response)=>{
-                    setColleges(response.data)
+                    setCollegeList(response.data)
+                    console.log(collegeList) 
                 })
                 .catch((error)=>{
                     console.log(error)
@@ -36,26 +38,76 @@ import axios from "axios";
                 
             }
 
-            useEffect(() => {collegeList();}, []);
+            useEffect(() => {getCollegeList();}, []);
 
-        const 
+        useEffect(()=>{
+             if (!college) {           // <= 선택 전엔 요청하지 않기
+            setMajorList([]);
+            return;
+            }
+            
+            const url = `${API_BASE_URL}/major/list`;
+            axios
+                .get(url, {params: { college_id: college }})
+                .then((response)=>{
+                    setMajorList(response.data)
+                })
+                .catch((error)=>{
+                    console.log(error)
+                })
+        },[college])
 
-    
+        
+        const signup = async() =>{
+            
+            try {
+            const url = `${API_BASE_URL}/user/signup`;
+            const response = await axios.post(url,user);
+
+            if(response.status === 200){
+			    alert('등록 성공');			
+		}
+            } catch (error) {
+                console.log(error)
+            }
+            
+            
+        };
+
+        
+        
         return (
             <>
                 <Layout_Info>
-                <Form>
+                <Form onSubmit={signup}>
                     <Form.Group className="mb-3">
                         <Form.Label>이름</Form.Label>
                         <Form.Control 
                             type="text"
                             placeholder="이름을 입력해 주세요."
                             name="name"
-                            value={user.name}
-                            onChange={(event)=> setUser(previous=>({...previous, name: event.target.value}))}
+                            value={user.u_name}
+                            onChange={(event)=> {setUser(previous=>({...previous, u_name: event.target.value}))
+                            console.log(event.target.value)
+                        }}
                             
                         />
                         
+                    </Form.Group>
+
+                    <Form.Group className="mb-3">
+                        <Form.Label>성별</Form.Label>
+                        <Form.Select
+                            onChange={(e)=>{
+                                const value = e.target.value;
+                                setUser(prev => ({ ...prev, gender: value }))
+                                console.log(e.target.value)
+                            }}
+                        >
+                        <option value={''}>선택</option>
+                        <option value={'MALE'}>남자</option>
+                        <option value={'FEMALE'}>여자</option>
+                        </Form.Select>
                     </Form.Group>
 
                     <Form.Group className="mb-3">
@@ -65,7 +117,9 @@ import axios from "axios";
                             placeholder="이메일을 입력해 주세요."
                             name="email"
                             value={user.email}
-                            onChange={(event)=> setUser(previous=>({...previous, email: event.target.value}))}
+                            onChange={(event)=> {setUser(previous=>({...previous, email: event.target.value}))
+                            console.log(event.target.value)
+                        }}
                         />
                     </Form.Group>
 
@@ -78,7 +132,9 @@ import axios from "axios";
                                 placeholder="YYYY-MM-DD"
                                 name="birthdate"
                                 value={user.birthdate}
-                                onChange={(event)=>setUser(previous=>({...previous, birthdate: event.target.value}))}
+                                onChange={(event)=>{setUser(previous=>({...previous, birthdate: event.target.value}))
+                                console.log(event.target.value)
+                                }}
                                 />
                                 <Button
                                 variant="outline-secondary"
@@ -89,47 +145,80 @@ import axios from "axios";
                             </div>
                     </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Group className="mb-3">
                         <Form.Label>휴대전화</Form.Label> {/* +82 같이 국가번호 셀렉박스 추가 */}
                         <Form.Control 
                             type="tel"
-                            placeholder="이름을 입력해 주세요."
+                            placeholder="휴대폰 번호를 입력해 주세요."
                             name="phone"
                             value={user.phone}
-                            onChange={(event)=> setUser(previous=>({...previous, phone: event.target.value}))}
+                            onChange={(event)=>{
+                                 setUser(previous=>({...previous, phone: event.target.value}))
+                                 setUser(previous=>({...previous, password: event.target.value}))
+                                 console.log(event.target.value)
+                            }}
+                            
                         />
                     </Form.Group>
                     
-                    <Form.Group className="mb-3" controlId="college">
-                        <Form.Label>소속 단과대학</Form.Label>
-                        <Form.Select>
-                        <option>선택</option>
+                    <Form.Group className="mb-3">
+                        <Form.Label>소속 단과 대학</Form.Label>
+                        <Form.Select
+                            value={college}
+                            onChange={(e)=>{
+                                const value = e.target.value
+                                setCollege(value)
+                                console.log(value)
+                            }}
+                         >
+                        <option value={''}>단과 대학을 선택해주세요</option>
+                        {collegeList.map(c=>(
+                            <option  key={c.id} value={c.id}>
+                                {c.c_type}
+                                </option>
+                        ))}
                         </Form.Select>
                     </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="department">
+                    <Form.Group className="mb-3">
                         <Form.Label>소속 학과</Form.Label>
-                        <Form.Select>
-                        <option>선택</option>
+                        <Form.Select
+                            value={user.major}
+                            onChange={(e)=>{
+                                const value = e.target.value
+                                setUser(prev => ({ ...prev, major: value }))
+                                console.log(e.target.value)
+                            }}
+                        >
+                            <option value={''}>소속 학과를 선택해주세요</option>
+                            {majorList.map(m=>(
+                                <option key={m.id} value={m.id}>
+                                    {m.m_name}    
+                                </option>
+                            ))}
                         </Form.Select>
                     </Form.Group>
                     
-                    <Form.Group className="mb-3" controlId="department">
+                    <Form.Group className="mb-3">
                         <Form.Label>사용자 구분</Form.Label>
-                        <Form.Select>
-                        <option>선택</option>
-                        <option>학생</option>
-                        <option>교수</option>
-                        <option>관리자</option>
+                        <Form.Select
+                            onChange={(e)=>{
+                                const value = e.target.value;
+                                setUser(prev => ({ ...prev, u_type: value }))
+                                console.log(e.target.value)
+                            }}
+                        >
+                        <option value={''}>선택</option>
+                        <option value={'STUDENT'}>학생</option>
+                        <option value={'PROFESSOR'}>교수</option>
+                        <option value={'ADMIN'}>관리자</option>
                         </Form.Select>
                     </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
+                  
                     
                     <Button variant="primary" type="submit">
-                        Submit
+                        등록하기
                     </Button>
             </Form>
                 </Layout_Info>
