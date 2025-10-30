6d89787ef2982ea3a931144f5db4c8ee418f2ca1
evenif99 <back86538774@gmail.com>
2025-10-30 18:24:03 +0900

    10/30 {update to github branch}

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -202,7 +202,7 @@ function App() {
                         
 
                         <Route path='/InfoHome' element={<InfoHome />} ></Route>
-                        <Route path='Student_Credit' element={<This_Credit />} ></Route>
+                        <Route path='/Student_Credit' element={<This_Credit />} ></Route>
                         
                         <Route path='/Change_Status' element={<Change_Status />} ></Route>
                         <Route path='/ChangeStatusList' element={<ChangeStatusList />} ></Route>
