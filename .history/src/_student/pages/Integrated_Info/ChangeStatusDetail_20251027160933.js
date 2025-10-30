ad926b18dbf203ea7be27e24c3b82dfed20664bd
evenif99 <back86538774@gmail.com>
2025-10-30 18:24:51 +0900

    10/30

--- .history/src/_student/pages/Integrated_Info/ChangeStatusDetail_20251027160933.js
+++
@@ -1,8 +0,0 @@
-function App() {
-    return (
-        <>
-            학적변경신청 상세보기
-        </>
-    )
-}
-export default App;
--- .history/src/_student/pages/Integrated_Info/ChangeStatusList_20251028164221.js
+++
@@ -1,8 +0,0 @@
-function App() {
-    return (
-        <>
-            학적변경신청리스트
-        </>
-    )
-}
-export default App;
